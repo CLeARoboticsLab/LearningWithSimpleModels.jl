2981487491e5f59c94dacaefdf943b6c3c7d54a8
jake-levy <jake.j.levy@gmail.com>
2023-02-06 12:37:26 -0600

    unicycle controller works

--- experiments/unicycle_controller.jl
+++ experiments/unicycle_controller.jl
@@ -16,10 +16,25 @@ function unicycle_policy(controller::Controller, state, setpoints)
     xdot_des = setpoints[3]
     ydot_des = setpoints[4]
 
-    xdot_tilde = xdot_des + controller.params.kx*(x_des - x)
-    ydot_tilde = ydot_des + controller.params.ky*(y_des - y)
+    xdot_tilde_des = xdot_des + controller.params.kx*(x_des - x)
+    ydot_tilde_des = ydot_des + controller.params.ky*(y_des - y)
     v_des = sqrt(xdot_des^2 + ydot_des^2)
-    ϕ_des = atan(ydot_tilde, xdot_tilde)
+
+    if xdot_tilde_des == 0
+        ϕ_des = sign(ydot_tilde_des)*π/2
+    else
+        ϕ_des = atan(ydot_tilde_des/xdot_tilde_des)
+    end
+
+    if xdot_tilde_des < 0.0
+        ϕ_des += π
+    elseif ydot_tilde_des < 0.0
+        ϕ_des += 2*π
+    end
+
+    if abs(ϕ_des - ϕ) > abs(ϕ_des - 2*π - ϕ)
+        ϕ_des -= 2*π
+    end
 
     a = controller.params.kv*(v_des - v)
     ω = controller.params.kϕ*(ϕ_des - ϕ)
@@ -39,6 +54,46 @@ unicycle_controller() = Controller(;
 
 
 function test_controller()
+    dynamics = unicycle_simple_dynamics()
     controller = unicycle_controller()
-    LearningWithSimpleModels.next_command(controller, ones(4), .5*ones(4))
+    time = 8.0
+    task = LearningWithSimpleModels.figure_eight(;xdot_f=0.0, ydot_f=0.0, time=time)
+    state0 = zeros(4)
+
+    ts = 0.0:0.01:time
+    state = state0
+    states = zeros(length(state0), length(ts))
+    setpoints = zeros(4, length(ts))
+    for (i,t) in enumerate(ts)
+        setpoint = collect(evaluate(task, t))
+        u = LearningWithSimpleModels.next_command(controller, state, setpoint)
+        state = LearningWithSimpleModels.f_simple(dynamics, t, state, u)
+        states[:,i] = state
+        setpoints[:,i] = setpoint
+    end
+    
+    xs = states[1,:]
+    ys = states[2,:]
+    vs = states[3,:]
+    ϕs = states[4,:]
+    xdes = setpoints[1,:]
+    ydes = setpoints[2,:]
+    xdot_des = setpoints[3,:]
+    ydot_des = setpoints[4,:]
+    vdes = sqrt.(xdot_des.^2 .+ ydot_des.^2)
+
+    fig = Figure(; resolution=(1000,800))
+    lines(fig[1:2,1:2], xs, ys, label ="actual"#=, xlabel="x", ylabel="y"=#)
+    lines!(fig[1:2,1:2], xdes, ydes, label ="desired"#=, xlabel="x", ylabel="y"=#)
+    lines(fig[3,1], ts, xs, label ="actual"#=, xlabel="t", ylabel="x"=#)
+    lines!(fig[3,1], ts, xdes, label ="desired"#=, xlabel="t", ylabel="x"=#)
+    lines(fig[3,2], ts, ys, label ="actual"#=, xlabel="t", ylabel="y"=#)
+    lines!(fig[3,2], ts, ydes, label ="desired"#=, xlabel="t", ylabel="y"=#)
+    lines(fig[4,1], ts, vs, label ="actual"#=, xlabel="t", ylabel="v"=#)
+    lines!(fig[4,1], ts, vdes, label ="desired"#=, xlabel="t", ylabel="v"=#)
+    # lines(fig[4,2], ts, ϕs, label ="actual"#=, xlabel="t", ylabel="ϕ"=#)
+    # lines!(fig[4,2], ts, ϕdes, label ="desired"#=, xlabel="t", ylabel="ϕ"=#)
+    display(GLMakie.Screen(), fig)
+
+    #TODO: add types to func sigs for f and policy funcs?
 end
--- experiments/unicycle_experiment.jl
+++ experiments/unicycle_experiment.jl
@@ -1,4 +1,5 @@
 using LearningWithSimpleModels
+using GLMakie
 
 include("unicycle_controller.jl")
 
