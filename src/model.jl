4d5809294f8a0c9660ba0c11c8719e7a990713f5
jake-levy <jake.j.levy@gmail.com>
2023-02-06 21:30:12 -0600

    getting updated setpoint from model

--- src/model.jl
+++ src/model.jl
@@ -12,3 +12,15 @@ function make_model(layer_sizes::Vector{<:Integer})
     end
     return Chain(layers...,)
 end
+
+function new_setpoint_from_model(
+    setpoint::Vector{Float64}, 
+    model::Chain, 
+    t::Float64, 
+    x::Vector{Float64},
+    task_time::Float64
+)
+    t_transformed = [cos(2*π*t/task_time), sin(2*π*t/task_time)]
+    setpoint_correction = model(vcat(x, t_transformed))
+    return setpoint + setpoint_correction
+end
