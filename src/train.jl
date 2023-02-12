26dc491ef0e175a266efd7eb6a9254d329d3f4a5
jake-levy <jake.j.levy@gmail.com>
2023-02-12 09:18:29 -0600

    break out gradient estimate

--- src/train.jl
+++ src/train.jl
@@ -33,7 +33,7 @@ end
 function policy_update!(
     task::Spline, 
     model::Chain,
-    optimizer, #TODO type needed
+    optimizer,
     simple_dynamics::Dynamics,
     actual_dynamics::Dynamics, 
     controller::Controller,
@@ -42,16 +42,34 @@ function policy_update!(
     sim_params::SimulationParameters
 )
     r = rollout_actual_dynamics(task, model, actual_dynamics, controller, sim_params)
-    task_time, n_segments, segment_length, _ = properties(task, sim_params)
+    loss, grads = gradient_estimate(
+        r, task, model, simple_dynamics, controller, cost, training_params, sim_params
+    )
+    update!(optimizer,model,grads[1])
+    return loss
+end
+
+function gradient_estimate(
+    r::RolloutData,
+    task::Spline, 
+    model::Chain,
+    simple_dynamics::Dynamics,
+    controller::Controller,
+    cost::Cost,
+    training_params::TrainingParameters,
+    sim_params::SimulationParameters
+)
+    task_time, _, segment_length, _ = properties(task, sim_params)
+    n_segments = length(r.t0_segs)
 
     window_start_idx = 1
     window_end_idx = window_start_idx + training_params.segs_in_window
-
+    
     loss, grads = withgradient(model) do model
         loss = 0.0
         while window_end_idx <= n_segments
             window = window_start_idx:window_end_idx-1
-
+    
             for j in window
                 # Here we are repeating the process found in rollout_actual_dynamics
                 # so that we can take derivatives through the simple_dynamics
@@ -60,7 +78,7 @@ function policy_update!(
                 tf_seg = r.t0_segs[j] + sim_params.model_dt - sim_params.dt
                 setpoint = evaluate(task, tf_seg)
                 new_setpoint = new_setpoint_from_model(sim_params, setpoint, model, r.t0_segs[j], r.x0_segs[:,j], task_time)
-
+    
                 ts = r.t0_segs[j]:sim_params.dt:tf_seg
                 for (i,t) in enumerate(ts)
                     overall_idx = (j-1)*segment_length + i
@@ -79,6 +97,5 @@ function policy_update!(
         end
         return loss
     end
-    update!(optimizer,model,grads[1])
-    return loss
+    return loss, grads
 end
