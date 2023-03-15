85d024e7d19a4b574ff34f2ea1377b7db312c18b
jake-levy <jake.j.levy@gmail.com>
2023-03-15 18:02:13 -0500

    create subpkg to fix ci

--- experiments/jetracer/src/JetRacerExperiment.jl
+++ experiments/jetracer/src/JetRacerExperiment.jl
@@ -1,6 +1,7 @@
 # To use with Revise.jl:
 # using Revise
-# push!(LOAD_PATH, joinpath(pwd(), "experiments\\jetracer\\"))
+# push!(LOAD_PATH, joinpath(pwd(), "experiments\\jetracer\\src"))
+## activate experiments\\jetracer\\
 # using JetRacerExperiment
 
 module JetRacerExperiment
