99008bc76d0ae2653ca4b0e6a50a220da7de4413
jake-levy <jake.j.levy@gmail.com>
2023-03-16 18:48:47 -0700

    rollout data node

--- experiments/jetracer/lwsm_jetracer/src/rollout_data.cpp
+++ experiments/jetracer/lwsm_jetracer/src/rollout_data.cpp
@@ -3,27 +3,36 @@
 #include <ros/ros.h>
 
 #include "lwsm_jetracer/rollout_data_server.hpp"
-
-#include <ros_sockets/server.hpp>
+#include "lwsm_jetracer/RolloutData.h"
 
 class RolloutData
 {
   public:
-
     RolloutData()
     {
+      int port;
+      nh_.getParam("rollout_data/port", port);
 
+      ROS_INFO_STREAM("Starting rollout data server on port " << port);
+      rollout_data_server_ = new communication::RolloutDataServer(port);
+
+      sub_ = nh_.subscribe("jetracer/rollout_data", 100, &RolloutData::sub_callback, this);
     }
 
     ~RolloutData()
     {
-
+      delete rollout_data_server_;
     }
 
   private:
-
-    // ROS objects
     ros::NodeHandle nh_;
+    ros::Subscriber sub_;
+    communication::RolloutDataServer *rollout_data_server_;
+
+    void sub_callback(lwsm_jetracer::RolloutData data)
+    {
+      rollout_data_server_->update(data);
+    }
 
 };
 
