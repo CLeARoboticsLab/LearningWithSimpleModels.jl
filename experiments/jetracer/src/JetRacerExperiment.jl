f09998afc298c32ecda61556b4d55654909c0b89
jake-levy <jake.j.levy@gmail.com>
2023-03-18 09:17:29 -0500

    getting ready for jetracer hardware

--- experiments/jetracer/src/JetRacerExperiment.jl
+++ experiments/jetracer/src/JetRacerExperiment.jl
@@ -2,6 +2,9 @@
 # using Revise
 # push!(LOAD_PATH, joinpath(pwd(), "experiments\\jetracer\\src"))
 # ] activate experiments\\jetracer\\
+## First time only:
+## dev .
+## instantiate
 # using JetRacerExperiment
 
 module JetRacerExperiment
