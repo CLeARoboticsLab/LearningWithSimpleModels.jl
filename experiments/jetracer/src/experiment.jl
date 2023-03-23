a8c334e4613fe606d3d32a39042908c4b62069bb
jake-levy <jake.j.levy@gmail.com>
2023-03-23 16:43:27 -0500

    trying parameters....

--- experiments/jetracer/src/experiment.jl
+++ experiments/jetracer/src/experiment.jl
@@ -14,7 +14,7 @@ Base.@kwdef struct JetracerCostParameters <: CostParameters
 end
 
 jetracer_cost() = Cost(;
-    params = JetracerCostParameters(; input_weight = 0.01),
+    params = JetracerCostParameters(; input_weight = 0.001),
     g = (cost::Cost, x::Vector{Float64}, x_des::Vector{Float64}, u::Vector{Float64}) -> begin
         return (
             (x[1] - x_des[1])^2 + (x[2] - x_des[2])^2 #+ 3.0*(x[3] - sqrt(x_des[3]^2 + x_des[4]^2))^2
@@ -31,7 +31,7 @@ jetracer_figure_eight_task() = figure_eight(;
     xdot_f = nothing,
     ydot_f = nothing,
     radius = 1.50,
-    time = 6.0,
+    time = 8.0,
     laps = 1
 )
 
@@ -49,9 +49,9 @@ Base.show(io::IO, p::HardwareTrainingAlgorithm) = print(io,
 )
 
 jetracer_training_algorithm() = HardwareTrainingAlgorithm(;
-    seconds_per_rollout = 5 + 6.0*1.00,
-    n_beginning_segs_to_truncate = 10,
-    segs_in_window = 10,
+    seconds_per_rollout = 3 + 6.0*2.00,
+    n_beginning_segs_to_truncate = 20,
+    segs_in_window = 20,
     stopping_segments = 2
 )
 
@@ -59,7 +59,7 @@ jetracer_training_parameters() = TrainingParameters(;
     name = "jetracer",
     save_path = ".data",
     hidden_layer_sizes = [64, 64],
-    learning_rate = 5e-4,
+    learning_rate = 2.5e-4,
     iters = 10,
     optim = gradient_descent,
     loss_aggregation = simulation_timestep,
@@ -72,14 +72,14 @@ jetracer_simulation_parameters() = SimulationParameters(;
     x0 = [0.0, 0.0, 0.0, 0.0],
     n_inputs = 2,
     dt = 1.0/50.0, # should match controller update rate
-    model_dt = 6.0/20,
-    model_scale = [1.0, 1.0, 1.0, 1.0, 0.5, 0.5, 0.5, 0.5]
+    model_dt = 6.0/20/2,
+    model_scale = [2.0, 2.0, 2.0, 2.0, 0.5, 0.5, 0.5, 0.5]
 )
 
 jetracer_evaluation_parameters() = EvaluationParameters(;
     name = "jetracer",
     path = ".data",    
-    n_task_executions = 1,
+    n_task_executions = 3,
     save_plot = true,
     save_animation = true
 )
