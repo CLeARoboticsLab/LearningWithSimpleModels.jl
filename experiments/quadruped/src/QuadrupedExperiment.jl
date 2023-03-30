f10d01d1357e401c656038922ae31d4923b24e18
jake-levy <jake.j.levy@gmail.com>
2023-03-29 22:14:29 -0500

    add subpkg

---
+++ experiments/quadruped/src/QuadrupedExperiment.jl
@@ -0,0 +1,14 @@
+# To use with Revise.jl:
+# using Revise
+# push!(LOAD_PATH, joinpath(pwd(), "experiments\\quadruped\\src"))
+# ] activate experiments\\quadruped\\
+## First time only:
+## dev .
+## instantiate
+# using QuadrupedExperiment
+
+module QuadrupedExperiment
+
+using LearningWithSimpleModels
+
+end #module
