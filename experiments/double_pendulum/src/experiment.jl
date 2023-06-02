90cc7193d3f4d04fb63e762190e2bc68d21725ea
jake-levy <jake.j.levy@gmail.com>
2023-06-02 11:28:42 -0500

    use manipulator equations

--- experiments/double_pendulum/src/experiment.jl
+++ experiments/double_pendulum/src/experiment.jl
@@ -1,41 +1,70 @@
+struct DoublePendulumDynamicsParameters <: DyanmicsParameters
+    m1
+    m2
+    l1
+    l2
+    g
+end
+
+dp_actual_dynamics_params() = DoublePendulumDynamicsParameters(1, 1, 1, 1, 9.81)
+dp_simple_dynamics_params() = dp_actual_dynamics_params()
+
+H(q, p::DoublePendulumDynamicsParameters) = [
+    (p.m1 + p.m2)*p.l1^2 + p.m2*p.l2^2 + 2*p.m2*p.l1*p.l2*cos(q[2])     p.m2*p.l2^2 + p.m2*p.l1*p.l2*cos(q[2])
+    p.m2*p.l2^2 + p.m2*p.l1*p.l2*cos(q[2])                              p.m2*p.l2^2
+]
+
+C(q, qdot, p::DoublePendulumDynamicsParameters) = [
+    0                                   -p.m2*p.l1*p.l2*(2*qdot[1] + qdot[2])*sin(q[2])
+    p.m2*p.l1*p.l2*qdot[1]*sin(q[2])    0
+]
+
+G(q, p::DoublePendulumDynamicsParameters) = p.g * [
+    (p.m1 + p.m2)*p.l1*sin(q[1]) + p.m2*p.l2*sin(q[1]+q[2])
+    p.m2*p.l2*sin(q[1]+q[2])
+]
+
+B(q, p::DoublePendulumDynamicsParameters) = [
+    1   0
+    0   1
+]
+
+function dp_actual_dynamics_f(dyn::Dynamics, t::Float64, dt::Float64, x::Vector{Float64}, u::Vector{Float64})
+    # x[1] = θ₁
+    # x[2] = θ₂
+    # x[3] = θ₁_dot
+    # x[4] = θ₂_dot
+    
+    q = [ x[1], x[2] ]
+    qdot = [ x[3], x[4] ]
+    p = dyn.params
+
+    x1dot = x[3]
+    x2dot = x[4]
+
+    qddot = inv(H(q,p))*( B(q,p)*u - C(q,qdot,p)*qdot - G(q,p) )
+    x3dot = qddot[1]
+    x4dot = qddot[2]
+
+    return [ # euler integration
+        x[1] + x1dot*dt
+        x[2] + x2dot*dt
+        x[3] + x3dot*dt
+        x[4] + x4dot*dt
+    ]
+end
+
+dp_simple_dynamics_f(dyn::Dynamics, t::Float64, dt::Float64, 
+    x::Vector{Float64}, u::Vector{Float64}) = dp_actual_dynamics_f(dyn,t,dt,x,u)
+
 dp_simple_dynamics() = Dynamics(;
-    f = (dyn::Dynamics, t::Float64, dt::Float64, x::Vector{Float64}, u::Vector{Float64}) -> begin
-        m1 = 1
-        m2 = 1
-        g = 9.81
-        l1 = 1
-        l2 = 1
-        
-        M = m1 + m2
-        Δθ = x[1] - x[2]
-        α = m1 + m2*sin(Δθ)^2
-        return [
-            x[1] + x[3]*dt,
-            x[2] + x[4]*dt,
-            x[3] + ((l1*α)^(-1)*(-sin(Δθ)*(m2*l1*x[3]^2*cos(Δθ) + m2*l2*x[4]^2) - g*( M*sin(x[1]) - m2*sin(x[2])*cos(Δθ))) + u[1])*dt,
-            x[4] + ((l2*α)^(-1)*( sin(Δθ)*(m2*l2*x[4]^2*cos(Δθ) +  M*l1*x[3]^2) + g*(-M*sin(x[2]) +  M*sin(x[1])*cos(Δθ))) + u[2])*dt,
-        ]
-    end
+    params = dp_simple_dynamics_params(),
+    f = dp_simple_dynamics_f
 )
 
 dp_actual_dynamics() = Dynamics(;
-    f = (dyn::Dynamics, t::Float64, dt::Float64, x::Vector{Float64}, u::Vector{Float64}) -> begin
-        m1 = 1
-        m2 = 1
-        g = 9.81
-        l1 = 1
-        l2 = 1
-        
-        M = m1 + m2
-        Δθ = x[1] - x[2]
-        α = m1 + m2*sin(Δθ)^2
-        return [
-            x[1] + x[3]*dt,
-            x[2] + x[4]*dt,
-            x[3] + ((l1*α)^(-1)*(-sin(Δθ)*(m2*l1*x[3]^2*cos(Δθ) + m2*l2*x[4]^2) - g*( M*sin(x[1]) - m2*sin(x[2])*cos(Δθ))) + u[1])*dt,
-            x[4] + ((l2*α)^(-1)*( sin(Δθ)*(m2*l2*x[4]^2*cos(Δθ) +  M*l1*x[3]^2) + g*(-M*sin(x[2]) +  M*sin(x[1])*cos(Δθ))) + u[2])*dt,
-        ]
-    end
+    params = dp_actual_dynamics_params(),
+    f = dp_actual_dynamics_f
 )
 
 Base.@kwdef struct DpCostParameters <: CostParameters
@@ -78,7 +107,7 @@ dp_training_parameters() = TrainingParameters(; # TODO
 )
 
 dp_simulation_parameters() = SimulationParameters(;
-    x0 = [π/2-0.1, π/2-0.1, 0.0, 0.0],
+    x0 = [π/8, π/8, 0.0, 0.0],
     n_inputs = 2,
     dt = 0.01, # should match controller update rate
     model_dt = m_dt(),
@@ -86,7 +115,7 @@ dp_simulation_parameters() = SimulationParameters(;
     #                                  kx    ky    kvx   kϕ    kvy   kω
 )
 
-dp_evaluation_parameters() = EvaluationParameters(;
+dp_evaluation_parameters() = EvaluationParameters(; #add here
     name = "dp_sim",
     path = ".data",    
     n_task_executions = 1,
